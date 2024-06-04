6d40f366088d6f86ba4c8263e1e29f4319c709b2
mugurel1998 <mugurelc12@gmail.com>
2024-06-05 02:03:46 +0300

    Add test suite

--- cypress/e2e/color-wheel-spec.cy.js
+++ cypress/e2e/color-wheel-spec.cy.js
@@ -1,5 +1,18 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+
+describe("Color Wheel test suite", () => {
+	before(() => {
+		cy.visit("/color-wheel");
+	});
+	
+	it("Roll the wheel", () => {
+		
+	});
+
+	it("Pick a wrong color", () => {
+		
+	});
+	
+	it("Play 3 times", () => {
+		
+	});
+});
